497ef8ac2cc05257bf446f2df9c6ccb0b2f88ae3
code-enjoyer-bob <160259869+code-enjoyer-bob@users.noreply.github.com>
2024-02-17 23:51:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello, welcome to my program. I will be interviewing you today. ")
+name = input("What is your name? ")
+food = input("What is your favorite food? ")
+fav_music = input("What is your favorite music? ")
+living_location = input("Where do you live?")
+print()
+print("So you are " + name + ", you like to eat " + food +
+      ", your favorite music is " + fav_music + ", and you live in " +
+      living_location + ".")
+print()
+print("That's amazing!")
