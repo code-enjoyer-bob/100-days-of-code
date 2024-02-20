e3473a7f2346796091956fa44ab5d4c31a47c727
code-enjoyer-bob <160259869+code-enjoyer-bob@users.noreply.github.com>
2024-02-20 19:26:16 +0100

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,21 @@
+gender = input("Are you a male or female? ")
+print()
+print("Interesting...")
+print()
+age = input("Are you young or old? ")
+print()
+print("I don't believe you!")
+print()
+if gender == "male":
+  print("You are strong")
+else:
+  print("You are smart")
+if age == "young":
+  print("You can learn quickly")
+else:
+  print("You have a lot of experience")
+like_red = input("Do you like the color red? ")
+if like_red == "yes":
+  print("Liking red is for passionate people, you are passionate")
+else:
+  print("You are not passionate, because you don't like red")
